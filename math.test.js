fb0bcba9a6b65c45b53ad3b35155ee44f2723088
rashedrk <rashedrk001@gmail.com>
2026-01-05 01:03:52 +0600

    multiple test & use AAA method

--- math.test.js
+++ math.test.js
@@ -2,6 +2,34 @@ import { it, expect } from 'vitest'
 import { add } from './math'
 
 it('should return the correct sum if an array of numbers are provided', () => {
-    const result = add([1,2,3])
-    expect(result).toBe(6)
-})
+    // Arrange 
+    const numbers = [1,2,3];
+    const expectedResult = numbers.reduce((a,b) => a + b, 0);
+
+    // Actions
+    const result = add(numbers)
+
+    // Assertion
+    expect(result).toBe(expectedResult)
+});
+it('should return NaN if any element of array is invalid', () => {
+    // Arrange 
+    const numbers = [1,"s",3];
+
+    // Actions
+    const result = add(numbers)
+
+    // Assertion
+    expect(result).toBeNaN()
+});
+it('should return the correct sum if an array of numeric strings are provided', () => {
+    // Arrange 
+    const numbers = ['1','2','3'];
+    const expectedResult = numbers.reduce((a,b) => +a + +b, 0);
+
+    // Actions
+    const result = add(numbers)
+
+    // Assertion
+    expect(result).toBe(expectedResult)
+});
